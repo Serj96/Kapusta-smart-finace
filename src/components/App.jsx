0c67286af0bfcaac8839c92825044e61d421d84c
QNeee <vovasagan7@gmail.com>
2023-01-29 10:37:18 +0300

    fix-slice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,12 @@
 
 export const App = () => {
-  return
+  // return <Routes>
+  //   <Route path="/home" element={<Layout />}>
+  //     <Route index element={!token ? <Login /> : <Navigate to={"/home/expenses"} replace />} />
+  //     <Route path="register" element={!token ? <Register /> : <Navigate to={"/home/expenses"} replace />} />
+  //     <Route path="expenses" element={token ? <Expenses /> : <Navigate to={"/home"} replace />} />
+  //     <Route path="incomes" element={token ? <Incomes /> : <Navigate to={"/home"} replace />} />
+  //   </Route>
+  //   <Route path="*" element={<Navigate to={"/home"} />} />
+  // </Routes>
 };
