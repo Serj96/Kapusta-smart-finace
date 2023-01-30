e72dcd37886b3d5ed0e385cb22082216d54162bd
QNeee <vovasagan7@gmail.com>
2023-01-30 11:00:57 +0300

    dev general

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import HomePage from './HomePage/HomePage';
-import { Container } from './App.styled';
 import { useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import { getSid } from 'Redux/kapustaSlice';
 import { Registration } from './Registration/Registration';
 import { Navigate } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
+import AppBarReport from './Report/AppBarReport/AppBarReport'
+import Expense from 'pages/Report/Expenses/Expenses';
+import Income from 'pages/Report/Income/Income';
+import ExpensesPage from './ExpensesPage/ExpensesPage';
+import IncomePage from './IncomePage/IncomePage';
 export const App = () => {
   const token = useSelector(getSid);
   return (
@@ -29,8 +33,19 @@ export const App = () => {
         />
         <Route
           path="expenses"
-          element={token ? <div>Expenses</div> : <Navigate to={'/home'} />}
+          element={token ? <ExpensesPage /> : <Navigate to={'/home'} />}
         />
+        <Route
+          path="income"
+          element={token ? <IncomePage /> : <Navigate to={'/home'} />}
+        />
+        <Route path='reports' element={<AppBarReport />} >
+          <Route index element={<Expense />} />
+          <Route
+            path="income"
+            element={token ? <Income /> : <Navigate to={'/home'} />}
+          />
+        </Route>
       </Route>
       <Route path="*" element={<Navigate to={'/home'} />} />
     </Routes>
--- src/components/Expenses/NavLink/NavLink.jsx
+++ src/components/Expenses/NavLink/NavLink.jsx
@@ -6,8 +6,8 @@ import { NavItem, NavList } from './NavLink.styled';
 const NavLink = () => {
   const NavItems = useMemo(
     () => [
-      { href: '/expenses', text: 'EXPENSES' },
-      { href: '/income', text: 'INCOME' },
+      { href: '/home/expenses', text: 'EXPENSES' },
+      { href: '/home/income', text: 'INCOME' },
     ],
     []
   );
