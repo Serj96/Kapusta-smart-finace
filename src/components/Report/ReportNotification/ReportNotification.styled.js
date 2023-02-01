c836db950e434f30a71cc2af566dfa4102da1e34
Olha-Suchok <suchok_olya@ukr.net>
2023-02-01 16:57:53 +0200

    Fixed styled

---
+++ src/components/Report/ReportNotification/ReportNotification.styled.js
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const ReportExpenceNotificationText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: #52555f;
+
+  font-family: 'Roboto';
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 0.9;
+
+  @media (min-width: 768px) {
+  }
+
+  @media (min-width: 768px) and (max-width: 1279.98px) {
+  }
+
+  @media (min-width: 1280px) {
+  }
+`;
--- src/components/ReportIcons/Icons.styled.js
+++ src/components/ReportIcons/Icons.styled.js
@@ -3,6 +3,11 @@ import styled from '@emotion/styled';
 export const ReportExpenseListItemSvg = styled.svg`
   fill: #071f41;
   transition: fill 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover,
+  &:focus {
+    fill: #ff751d;
+  }
 `;
 
 export const ReportBackgroundSvg = styled.div`
