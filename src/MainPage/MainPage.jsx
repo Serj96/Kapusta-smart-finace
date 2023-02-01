0f99dd1ad4423237ad215bfbd33622989632c5c2
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-02-01 14:38:12 +0200

    ,

--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -1,6 +1,5 @@
 
 import { MainSt, Top } from './MainPage.styled';
-// import * as Styled from './MainPage.styled';
 import Container from '../Container/Container';
 
 export const MainPage = ({ children }) => {
@@ -8,10 +7,6 @@ export const MainPage = ({ children }) => {
 
   return (
     <>
-      {/* <Styled.MainAuth>
-        <Styled.TopAuth></Styled.TopAuth>
-        <Container>{children}</Container>
-      </Styled.MainAuth> */}
       <MainSt>
         <Top></Top>
         <Container>{children}</Container>
