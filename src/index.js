40158880b3566188eff692e2119cd7fbe8152ce8
QNeee <vovasagan7@gmail.com>
2023-02-02 19:07:26 +0300

    end

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/Kapusta-team-project-react'>
       <App />
     </BrowserRouter>
   </Provider>
